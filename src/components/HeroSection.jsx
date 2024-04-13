1e471af14c008b5a65987c34306d3dd6f6caffab
Ngumbutlar <ngumbutlar43@gmail.com>
2024-04-13 13:38:16 +0100

    Added Hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { IoSearchOutline } from 'react-icons/io5'
+import Pattern from '../assets/images/Background.jpg'
+
+const HeroSection = () => {
+  return (
+    <section className='bg-[var(--bg-color)] '>
+        <div className='flex flex-col items-center'>
+           <p className='bg-[var(--color-light)] text-[var(--color-purple)] pb-2 px-4 p-2 rounded-sm md:rounded-lg cursor-pointer mt-7 text-sm'>Our blog</p>
+           <h1 className='text-[var(--color-primary)] mb-4 text-4xl font-[600]'>Resources and insigts</h1>
+           <p className='text-[var(--color-purple)] mb-4'> The latest industry news, interview, technologies, and resources.</p>
+           <div className="bg-white shadow-sm p-3 outline-none rounded-lg  mx-[25%] flex items-center">
+          <IoSearchOutline className="text-[20px] text-gray-400" />
+          <input type="text" placeholder="Search" className="ml-2" />
+        </div>
+        <div className='w-full'>
+            <img src={Pattern} alt="" />
+        </div>
+        </div>
+        
+    </section>
+  )
+}
+
+export default HeroSection
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import BlogPost from '../components/BlogPost'
+
+const Home = () => {
+  return (
+    <div>
+        <HeroSection/>
+
+        <BlogPost/>
+        
+    </div>
+  )
+}
+
+export default Home
