c3351c7c635dc4229b76bac05dd0191050f2d6db
Ngumbutlar <ngumbutlar43@gmail.com>
2024-04-23 11:32:52 +0100

    Added Posts to Blog section

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import { DUMMY_POSTS } from "../data";
+import PostItem from "./PostItem";
 
 const BlogPost = () => {
+  const [posts, setPosts] = useState(DUMMY_POSTS);
   return (
-    <div>BlogPost</div>
-  )
-}
+    <section className="posts grid grid-cols-1 md:grid-cols-3 mt-10 px-10 md:px-15 lg:px-32">
+      {posts.map(
+        ({id ,thumbnail, category, title, desc, authorId }, index) => (
+          <PostItem
+            postId = {id}
+            key={index}
+            thumbnail={thumbnail}
+            category={category}
+            title={title}
+            description={desc}
+            authorId={authorId}
+          />
+        )
+      )}
+    </section>
+  );
+};
 
-export default BlogPost
+export default BlogPost;
