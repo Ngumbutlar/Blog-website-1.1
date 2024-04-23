b2676514338355159ce31bfdd4d27369960432fa
Ngumbutlar <ngumbutlar43@gmail.com>
2024-04-23 11:32:32 +0100

    Added author avatar images

---
+++ src/data.js
@@ -0,0 +1,32 @@
+import Thumbnail1 from './assets/images/image1.jpg'
+import Thumbnail2 from './assets/images/image2.jpg'
+import Thumbnail3 from './assets/images/image3.jpg'
+
+export const DUMMY_POSTS = [
+    {
+        id: '1',
+        thumbnail: Thumbnail1,
+        category: 'Design',
+        title: 'UX Reviewa Presentation',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis recusandae libero quisquam animi quos veniam nihil necessitatibus saepe doloremque, voluptatum, cumque quas facilis accusantium minima. Labore nam dolor asperiores?',
+        authorID: 3
+      },
+
+      {
+        id: '2',
+        thumbnail: Thumbnail2,
+        category: 'Product',
+        title: 'Migrating to Linear 101',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis recusandae libero quisquam animi quos veniam nihil necessitatibus saepe doloremque, voluptatum, cumque quas facilis accusantium minima. Labore nam dolor asperiores?',
+        authorID: 2
+      },
+
+      {
+        id: '3',
+        thumbnail: Thumbnail3,
+        category: 'Software Engineering',
+        title: 'Building your API Stack',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis recusandae libero quisquam animi quos veniam nihil necessitatibus saepe doloremque, voluptatum, cumque quas facilis accusantium minima. Labore nam dolor asperiores?',
+        authorID: 2
+      },
+]
