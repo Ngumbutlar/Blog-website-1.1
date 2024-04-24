53d2a65195ddfdbc38f7457d686a232ccdaf0115
Ngumbutlar <ngumbutlar43@gmail.com>
2024-04-24 09:33:09 +0100

    Added Json file

--- src/data.js
+++ src/data.js
@@ -1,13 +1,20 @@
 import Thumbnail1 from './assets/images/image1.jpg'
 import Thumbnail2 from './assets/images/image2.jpg'
 import Thumbnail3 from './assets/images/image3.jpg'
+import Thumbnail4 from './assets/images/image4.jpg'
+import Thumbnail5 from './assets/images/image5.jpg'
+import Thumbnail6 from './assets/images/image6.jpg'
+import Thumbnail7 from './assets/images/image7.jpg'
+import Thumbnail8 from './assets/images/image8.jpg'
+import Thumbnail9 from './assets/images/image9.jpg'
+
 
 export const DUMMY_POSTS = [
     {
         id: '1',
         thumbnail: Thumbnail1,
         category: 'Design',
-        title: 'UX Reviewa Presentation',
+        title: 'UX Review Presentation',
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis recusandae libero quisquam animi quos veniam nihil necessitatibus saepe doloremque, voluptatum, cumque quas facilis accusantium minima. Labore nam dolor asperiores?',
         authorID: 3
       },
@@ -29,4 +36,59 @@ export const DUMMY_POSTS = [
         desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis recusandae libero quisquam animi quos veniam nihil necessitatibus saepe doloremque, voluptatum, cumque quas facilis accusantium minima. Labore nam dolor asperiores?',
         authorID: 2
       },
+      {
+        id: '4',
+        thumbnail: Thumbnail4,
+        category: 'Bill Walsh Leadership Lessons',
+        title: 'Building your API Stack',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis recusandae libero quisquam animi quos veniam nihil necessitatibus saepe doloremque, voluptatum, cumque quas facilis accusantium minima. Labore nam dolor asperiores?',
+        authorID: 2
+      },
+
+      {
+        id: '5',
+        thumbnail: Thumbnail5,
+        category: 'Bill Walsh Leadership Lessons',
+        title: 'Building your API Stack',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis recusandae libero quisquam animi quos veniam nihil necessitatibus saepe doloremque, voluptatum, cumque quas facilis accusantium minima. Labore nam dolor asperiores?',
+        authorID: 2
+      },
+      
+      {
+        id: '6',
+        thumbnail: Thumbnail6,
+        category: 'Bill Walsh Leadership Lessons',
+        title: 'Building your API Stack',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis recusandae libero quisquam animi quos veniam nihil necessitatibus saepe doloremque, voluptatum, cumque quas facilis accusantium minima. Labore nam dolor asperiores?',
+        authorID: 2
+      },
+      {
+        id: '7',
+        thumbnail: Thumbnail7,
+        category: 'Bill Walsh Leadership Lessons',
+        title: 'Building your API Stack',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis recusandae libero quisquam animi quos veniam nihil necessitatibus saepe doloremque, voluptatum, cumque quas facilis accusantium minima. Labore nam dolor asperiores?',
+        authorID: 2
+      },
+      {
+        id: '8',
+        thumbnail: Thumbnail8,
+        category: 'Bill Walsh Leadership Lessons',
+        title: 'Building your API Stack',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis recusandae libero quisquam animi quos veniam nihil necessitatibus saepe doloremque, voluptatum, cumque quas facilis accusantium minima. Labore nam dolor asperiores?',
+        authorID: 2
+      },
+      {
+        id: '9',
+        thumbnail: Thumbnail9,
+        category: 'Bill Walsh Leadership Lessons',
+        title: 'Building your API Stack',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quis recusandae libero quisquam animi quos veniam nihil necessitatibus saepe doloremque, voluptatum, cumque quas facilis accusantium minima. Labore nam dolor asperiores?',
+        authorID: 2
+      },
+      
+      
+      
+      
+      
 ]
