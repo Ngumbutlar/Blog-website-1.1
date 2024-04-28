0c7a6076e9fad4cd79191187e088c12a1dacc4dd
Ngumbutlar <ngumbutlar43@gmail.com>
2024-04-28 05:57:31 +0100

    Modified base url

--- src/Services/GlobalAPI.jsx
+++ src/Services/GlobalAPI.jsx
@@ -2,9 +2,7 @@ import axios from "axios"
 
 
 const BASE_URL='https://json-server-neon.vercel.app'
-const getPost = axios.get(BASE_URL + '/
-
-posts');
+const getPost = axios.get(BASE_URL + '/posts');
 const getPostById= (id) => axios.get(BASE_URL + '/posts/' + id)
 
 export default {
